c669d3bc2c54f224d2f3131292c9c4d6559c8970
dotainion <58013018+dotainion@users.noreply.github.com>
2025-05-24 04:06:26 -0400

    update

--- tools/security/SecurityRepository.php
+++ tools/security/SecurityRepository.php
@@ -33,7 +33,10 @@ class SecurityRepository extends Repository{
 
     public function listSecurity(array $where = []):Collector{
         $this->select(Setup::userTableName())
-            ->join()->inner('credential', 'id', 'user', 'id');
+            ->join()->inner('credential', 'id', 'user', 'id')
+            ->cursor()->join()->inner('role', 'userId', 'user', 'id')
+            ->cursor()->join()->inner('rolePermission', 'userId', 'user', 'id');
+            
         if(isset($where['id'])){
             $this->where()->eq('id', $this->uuid($where['id']));
         }
